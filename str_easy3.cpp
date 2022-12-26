6bb09befdb7879dee75e6cb4b9ae9b1a3e25f444
RAVAGER206 <114873479+RAVAGER206@users.noreply.github.com>
2022-12-26 20:51:30 +0300

    Update str_easy3.cpp

--- str_easy3.cpp
+++ str_easy3.cpp
@@ -11,31 +11,31 @@ obc = 0;
 ad = itc_bin_num(a);
 if (a >= 0){
 for (i = 0; i < b; i++)
-{                                                  //положительное
+{                              // v1         
 nch = nch * 10 + ad % 10;
 ad = ad / 10;
 if (nch == 0)
 l = l * 10;
-}
+}                             // v1.1
 nstr = perevorot(nch)*l;
 for (i = 0; i < b - itc_len_num(nstr); i++)
 cout << "0";
 cout <<  nstr;
-}
-else{                                                    //начало создания прямого кода
+}                             // v1.2
+else{                                   
 b--;
 for (i = 0; i < b; i++){
 nch = nch * 10 + ad % 10;
 ad = ad / 10;
 if (nch == 0)
 l = l * 10;
-}
+}                          // v1.3
 nstr = perevorot(nch)*l;
 cout << "pryamoy code: " << 1;
 for (i = 0; i < b - itc_len_num(nstr); i++)
 cout << "0";
 cout <<  nstr << endl << "reverse code: 1";
-l = 0;                                          //конец создания прямого кода, начало обратного
+l = 0;                                          
 for (i = 0; i < itc_len_num(nstr); i++){
 if (nstr % 10 == 0)
 obc = obc * 10 + 1;
@@ -43,7 +43,7 @@ else{
 obc = obc * 10;
 if (obc == 0)
 l = l * 10;
-}
+}                       // pere v2
 nstr = nstr / 10;
 }
 obc = perevorot(obc*l);
@@ -52,4 +52,4 @@ cout << "1";
 cout <<  obc;
 }
 return 0;
-}
+} // end
